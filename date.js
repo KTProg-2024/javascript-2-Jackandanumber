6fe161fc3d66b023f5d555930ef61f1055f06201
Jackandanumber <>
2021-05-27 17:45:55 +0000

    added buttons

--- date.js
+++ date.js
@@ -3,3 +3,13 @@ function getDate()
 	var dateString = new Date();
 	document.getElementById("date").innerHTML=dateString;
 }
+
+function blue()
+{
+	document.getElementById("date").style.color="blue";
+}
+
+function brown()
+{
+	document.getElementById("date").style.color="brown";
+}
